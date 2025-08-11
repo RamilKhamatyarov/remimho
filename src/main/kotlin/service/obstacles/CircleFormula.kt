dd37d516b260a14fc9a9b996d8f05d42501ac46e
RamilKhamatyarov <rraamil@gmail.com>
2025-08-11 22:16:40 +0300

    Add random showing up new blocking curves in random times (#57)
    Co-authored-by: ramil-khamatyarov <ramil.hamatyarov@yandex.ru>

--- src/main/kotlin/service/obstacles/CircleFormula.kt
+++ src/main/kotlin/service/obstacles/CircleFormula.kt
@@ -1,22 +1,28 @@
 package ru.rkhamatyarov.service.obstacles
 
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import ru.rkhamatyarov.model.GameState
 import ru.rkhamatyarov.model.Line
 import ru.rkhamatyarov.model.Point
 import ru.rkhamatyarov.service.Formula
 import kotlin.math.PI
 import kotlin.math.cos
+import kotlin.math.min
 import kotlin.math.sin
 
 @ApplicationScoped
 class CircleFormula : Formula {
+    @Inject
+    lateinit var gameState: GameState
+
     override val name = "Circle"
 
     override fun createLine(): Line {
         val line = Line(width = 3.0)
-        val centerX = 600.0
-        val centerY = 150.0
-        val radius = 40.0
+        val centerX = gameState.canvasWidth / 2
+        val centerY = gameState.canvasHeight / 2
+        val radius = min(gameState.canvasWidth, gameState.canvasHeight) * 0.3
 
         for (angle in 0..360 step 10) {
             val rad = angle * PI / 180
